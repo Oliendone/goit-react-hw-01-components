35755f1131d54c7539cd8b23ad197e8a548e1bca
Nikita <Zungarcheg@gmail.com>
2020-09-01 17:34:45 +0300

    fourth task done

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import Statistics from './components/Statistics/StatisticsSection/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import statisticalData from './components/Statistics/statistical-data.json';
 import friends from './components/FriendList/friends.json';
+import transactions from './components/TransactionHistory/transactions.json';
 import './base.css';
 
 ReactDOM.render(
@@ -12,6 +14,7 @@ ReactDOM.render(
     <App />
     <Statistics title="Upload stats" stats={statisticalData} />
     <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
   </div>,
   document.querySelector('#root'),
 );
