62b69a09211380a26a92eca86aad7d3a1ba3f035
Nikita <Zungarcheg@gmail.com>
2020-09-01 16:54:49 +0300

    third task done

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import Statistics from './components/Statistics/StatisticsSection/Statistics';
+import FriendList from './components/FriendList/FriendList';
 import statisticalData from './components/Statistics/statistical-data.json';
+import friends from './components/FriendList/friends.json';
 import './base.css';
 
 ReactDOM.render(
   <div>
     <App />
     <Statistics title="Upload stats" stats={statisticalData} />
+    <FriendList friends={friends} />
   </div>,
   document.querySelector('#root'),
 );
